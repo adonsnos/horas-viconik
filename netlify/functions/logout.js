08ac1c000f854ff04e2e116302abb0192b105459
adonsnos <122192410+adonsnos@users.noreply.github.com>
2026-07-26 13:36:38 +0200

    Delete netlify directory

--- netlify/functions/logout.js
+++
@@ -1,8 +0,0 @@
-export default async () => {
-  const headers = new Headers();
-  headers.set("Location", "/");
-  headers.append("Set-Cookie", "horas_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0");
-  return new Response(null, { status: 302, headers });
-};
-
-export const config = { path: "/.netlify/functions/logout" };
